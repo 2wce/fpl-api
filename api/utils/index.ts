3ae9a50d9f9f1e78b3f7fe6c5a2cab236dcb53da
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2020-02-28 14:02:10 +0200

    chore 🚨: remove unused interfaces

--- api/utils/index.ts
+++ api/utils/index.ts
@@ -1,14 +1,1 @@
-export { Context } from './context';
-export {
-    Standing,
-    Goal,
-    Player,
-    Club,
-    Fixture,
-    User,
-    Team,
-    Ground,
-    Score,
-    Match,
-    AssistStat,
-    GoalStat } from './interfaces';
+export { Context } from "./context";
