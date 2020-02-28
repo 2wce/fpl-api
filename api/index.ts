79d292df8d68859793e60669b0f84caac503b948
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2020-02-28 14:00:48 +0200

    feat 🎸: setup typeorm

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,28 @@
+// @ts-ignore
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
 import * as schema from "./schema";
 
-const server = new ApolloServer({
-  ...schema,
-  introspection: true,
-  playground: { endpoint: "/api" }
-});
+export default createConnection()
+  .then(async connection => {
+    const server = new ApolloServer({
+      ...schema,
+      introspection: true,
+      playground: { endpoint: "/api" }
+    });
 
-const app = express();
+    const app = express();
 
-server.applyMiddleware({ app, path: "/api" });
+    server.applyMiddleware({ app, path: "/api" });
 
-const port = process.env.PORT || 4000;
+    const port = process.env.PORT || 4000;
 
-app.listen({ port }, () =>
-  console.log(`🚀 Server ready at ${server.graphqlPath}`)
-);
+    app.listen({ port }, () =>
+      console.log(`🚀 Server ready at ${server.graphqlPath}`)
+    );
+  })
+  .catch(error => console.log("TypeORM connection error: ", error));
 
-export default app;
+// export default app;
