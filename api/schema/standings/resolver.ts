138a0616c508e7783d25b099387a221cb25c87ad
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2020-02-29 13:03:02 +0200

    feat 🎸: new on demand logic for standings query

--- api/schema/standings/resolver.ts
+++ api/schema/standings/resolver.ts
@@ -1,10 +1,34 @@
 import { ApolloError, ValidationError } from "apollo-server-express";
-import { standings } from "../../data";
+import { clubs, matches, standings } from "../../data";
 import { Context } from "../../utils";
+import { getNonRelationalStatsPerClub } from "./utils";
 
 const standingResolvers = {
   Query: {
+    // idea is to generate a log table on demand - possible optimisation might be to move this to a stored procedure?
     async standings(parent: any, args: any, context: Context, info: any) {
+      // iterate through clubs
+      const standings = clubs
+        .map(club => {
+          // find the matches the club has played in
+          const clubMatches = matches.filter(
+            match =>
+              match.homeTeam.abbr === club.abbr ||
+              match.awayTeam.abbr === club.abbr
+          );
+
+          // generate stats using helper function
+          const stats = getNonRelationalStatsPerClub(club, clubMatches);
+
+          // build log standing per club
+          // @TODO: find way to generate positions
+          return {
+            club,
+            ...stats
+          };
+        })
+        .sort((a, b) => b.points - a.points); // order by points from most to least. @TODO: if points are equal then consider goal difference then alphabetical order?
+
       return standings;
     },
     async standing(parent: any, args: any, context: Context, info: any) {
